c879355c25b54056e4d57baf48c739a37b23c7fd
brewchetta <chett.tiller@gmail.com>
2022-01-11 11:53:43 -0500

    post lecture

--- client/src/ChatRoom.js
+++ client/src/ChatRoom.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { createConsumer } from "@rails/actioncable"
+const consumer = createConsumer()
 
 function ChatRoom({user}) {
 
   const [messages, setMessages] = useState([])
   const [messageInput, setMessageInput] = useState('')
+  const [channel, setChannel] = useState(null)
+
+  useEffect(() => {
+    if (user) {
+      fetch('/messages')
+      .then(res => res.json())
+      .then(messages => setMessages(messages))
+    }
+  }, [user])
+
+  useEffect(() => {
+    if (user) {
+      const newChannel = consumer.subscriptions.create({ channel: "ChatChannel", room: "Breakfast Club" },
+      {
+        received: (data) => {
+          setMessages(oldMessages => [...oldMessages, data])
+        }
+      })
+
+      setChannel(newChannel)
+    }
+  }, [user])
 
   function handleMessageInputChange(e) {
     setMessageInput(e.target.value)
@@ -11,7 +35,7 @@ function ChatRoom({user}) {
 
   function handleSubmit(e) {
     e.preventDefault()
-    console.log("---Something happens here---");
+    channel.send({content: messageInput})
     setMessageInput('')
   }
 
@@ -20,11 +44,11 @@ function ChatRoom({user}) {
 
       <h3>Cat Chat Room:</h3>
 
-      {/* messages get mapped here */}
+      {messages.map((message, i) => <p key={i}>{message.content} - {message.created_at}</p>)}
 
       <form onSubmit={handleSubmit}>
 
-      <input type="text" value={messageInput} onChange={handleMessageInputChange} />
+        <input type="text" value={messageInput} onChange={handleMessageInputChange} />
 
       </form>
 
