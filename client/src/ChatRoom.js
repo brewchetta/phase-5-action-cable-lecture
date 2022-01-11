97900f5b19d9edc7d9a8570eb83d2f25ab74bcc7
brewchetta <chett.tiller@gmail.com>
2022-01-10 20:31:28 -0500

    lecture set up with client

--- client/src/ChatRoom.js
+++ client/src/ChatRoom.js
@@ -1,37 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import consumer from './consumer-service'
+import React, { useState } from 'react'
 
 function ChatRoom({user}) {
 
-  // subscribed channel
-  const [channel, setChannel] = useState(null)
-  // messages from server
   const [messages, setMessages] = useState([])
-  // form input
   const [messageInput, setMessageInput] = useState('')
-
-  // set chat channel when logged in (can also happen when chat component mounts)
-  useEffect(() => {
-    if (user && !channel) {
-
-      fetch('/messages')
-      .then(res => res.json())
-      .then(messages => {
-        setMessages(prev => [...messages, ...prev])
-      })
-
-      const chatChannel = consumer.subscriptions.create({ channel: "ChatChannel", room: "Red Room" }, {
-        received(data) {
-          addToMessages(data)
-        }
-      })
-      setChannel(chatChannel)
-    }
-  }, [user])
-
-  function addToMessages(data) {
-    setMessages(prev => [...prev, data])
-  }
 
   function handleMessageInputChange(e) {
     setMessageInput(e.target.value)
@@ -39,16 +11,16 @@ function ChatRoom({user}) {
 
   function handleSubmit(e) {
     e.preventDefault()
-    channel.send({body: messageInput})
+    console.log("---Something happens here---");
     setMessageInput('')
   }
 
   return (
     <div>
 
-      <h3>My Chat Room:</h3>
+      <h3>Cat Chat Room:</h3>
 
-      {messages.map(m => <p>{m.user?.name ? `${m.user?.name}: ` : null} {m.content}</p>)}
+      {/* messages get mapped here */}
 
       <form onSubmit={handleSubmit}>
 
